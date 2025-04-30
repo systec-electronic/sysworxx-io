1e8602af6e582fb28325eb57c7d8464a657d9978
Andreas Dinter <andreas.dinter@systec-electronic.com>
2025-04-30 10:33:42 +0200

    Bindings/sysworxx-io-js/Cargo.toml: Link libsysworxx_io.so statically
    Having it as a build-dependencies adds dependencies to libiio /
    libsensors to the host system which is not what we want to
    achieve here.
    
    Adding it as a normal dependency and specify dynamic linking via
    `build.rs` will not work as well. (like working for libctr700drv.so)
    When inspecting the generated .so file one can see that libsysworxx_io
    is still statically linked. This maybe related to the use of neon, not
    sure about this.
    
    Since we deploy Node-RED nodes without this library we should be fine for
    now by statically linking.
    
    Signed-off-by: Andreas Dinter <andreas.dinter@systec-electronic.com>

--- Bindings/sysworxx-io-js/build.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// SPDX-FileCopyrightText: 2025 SYS TEC electronic AG <https://www.systec-electronic.com/>
-
-fn main() {
-    println!("cargo:rustc-link-lib=sysworxx_io");
-}
